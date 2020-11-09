3f429d878e988d914c5e1ee463c530ba4775519c
332018 <72552532+332018@users.noreply.github.com>
2020-11-09 21:12:07 +0100

    kreiranje klase

---
+++ Exercises/Exercises/ExerciseResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    class ExerciseResult
+    {
+        public int id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+    }
+}
